dbe334754def55d6a5e5d9f7352f7a86039d7fb6
Seairth Jacobs <sjacobs@wrsystems.com>
2023-12-08 08:25:55 -0500

    Day 6

---
+++ day06/puzzle1.py
@@ -0,0 +1,35 @@
+import re
+
+numbers_pattern = re.compile(r"(\d+)")
+
+with open('input.txt', 'r') as f:
+    race_records = f.readlines()
+
+times = tuple(map(int, numbers_pattern.findall(race_records[0])))
+distances = tuple(map(int, numbers_pattern.findall(race_records[1])))
+
+
+calc_distance = lambda t, s: (s * (t - s))
+
+margin = 1
+
+for race, time in enumerate(times):
+    distance = distances[race]
+
+    min_delay = 0
+
+    while calc_distance(time, min_delay) < distance:
+        min_delay += 1
+
+    # max_delay = min_delay
+
+    # while calc_distance(time, max_delay) > distance:
+        #max_delay += 1
+
+    max_delay = time - min_delay + 1
+
+    # no need to subtract one because we're treating it as [min, max)
+
+    margin *= (max_delay - min_delay)
+
+print(margin)
