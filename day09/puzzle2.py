f3f7a4e66d319d018dc9a89eee31ea1576a6a6a3
Seairth Jacobs <sjacobs@wrsystems.com>
2023-12-09 21:52:09 -0500

    Day 9

---
+++ day09/puzzle2.py
@@ -0,0 +1,25 @@
+import numpy as np
+from functools import reduce
+
+
+sum_next_seq = 0
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        seq = [int(n) for n in line.split(' ')]
+
+        seqs = [seq]
+
+        while reduce(lambda t, s: t or s != 0, seqs[-1], False):
+            seqs.append(np.diff(seqs[-1]).tolist())
+
+        seqs.pop()
+        last_val = 0
+
+        while len(seqs):
+            seq = seqs.pop()
+            last_val = seq[0] - last_val
+
+        sum_next_seq += last_val
+
+print(sum_next_seq)
